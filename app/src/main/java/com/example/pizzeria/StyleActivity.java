383cb08dbd76ae05bc42f02bab4d35815b98cbda
Johnathan Chan <johnathan.chan@rutgers.edu>
2024-12-03 22:45:34 -0500

    Worked on the ordering pizza activity. I think it still needs to display toppings and a few tweaks on front end.

--- app/src/main/java/com/example/pizzeria/StyleActivity.java
+++ app/src/main/java/com/example/pizzeria/StyleActivity.java
@@ -3,6 +3,12 @@ package com.example.pizzeria;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +17,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class StyleActivity extends AppCompatActivity {
+
+    /** Radio groups corresponding to those in the xml */
+    private RadioGroup styleGroup, typeGroup, sizeGroup;
+
+    /** Button corresponding to the addToOrder button on the xml*/
+    private Button orderButton;
+
+    private TextView costText;
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +40,175 @@ public class StyleActivity extends AppCompatActivity {
             return insets;
         });
          */
+        this.styleGroup = findViewById(R.id.styleGroup);
+        this.typeGroup = findViewById(R.id.typeGroup);
+        this.sizeGroup = findViewById(R.id.sizeGroup);
+        this.orderButton = findViewById(R.id.addToOrder);
+        this.costText = findViewById(R.id.costText);
+        this.imageView = findViewById(R.id.imageView);
+
+        RadioGroup.OnCheckedChangeListener listener = (group, checkedId) -> fieldsFilled();
+        styleGroup.setOnCheckedChangeListener(listener);
+        typeGroup.setOnCheckedChangeListener(listener);
+        sizeGroup.setOnCheckedChangeListener(listener);
+    }
+
+    /**
+     * A callback method executed right after onCreate().
+     */
+    protected void onStart() {
+        super.onStart();
+        //Toast.makeText(this, "onStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * A callback method executed right after onStart().
+     */
+    protected void onResume() {
+        super.onResume();
+        //Toast.makeText(this, "onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Checks if the radio groups have values selected, enabling the order button if they all do
+     */
+    private void fieldsFilled() {
+        if (styleGroup.getCheckedRadioButtonId() != -1 &&
+                typeGroup.getCheckedRadioButtonId() != -1 &&
+                sizeGroup.getCheckedRadioButtonId() != -1) {
+            orderButton.setEnabled(true);
+            Pizza pizza = makePizza();
+            String cost = getString(R.string.pricing) + " $" + pizza.price();
+            costText.setText(cost);
+            updateImages();
+        } else {
+            orderButton.setEnabled(false);
+            costText.setText(R.string.finish_choosing_pizza_to_see_pricing);
+            updateImages();
+        }
+    }
+
+/**
+ * Update images displayed.
+ */
+    private void updateImages() {
+        if (styleGroup.getCheckedRadioButtonId() != -1 && typeGroup.getCheckedRadioButtonId() != -1){
+            fullImageSetter();
+        } else if (typeGroup.getCheckedRadioButtonId() != -1) {
+            typeImageSetter();
+        } else if (styleGroup.getCheckedRadioButtonId() != -1) {
+            styleImageSetter();
+        } else {
+            imageView.setImageResource(R.drawable.pizza);
+        }
+    }
+/**
+ * Set images based on style.
+ */
+
+    private void styleImageSetter(){
+        String style = ((RadioButton)findViewById(styleGroup.getCheckedRadioButtonId())).getText().toString();
+        if (style.equalsIgnoreCase(getString(R.string.new_york))){
+            imageView.setImageResource(R.drawable.ny);
+        } else if (style.equalsIgnoreCase(getString(R.string.chicago))) {
+            imageView.setImageResource(R.drawable.chicago);
+        } else {
+            Toast.makeText(this, getString(R.string.error), Toast.LENGTH_SHORT).show();
+            imageView.setImageResource(R.drawable.pizza);
+        }
+    }
+
+
+/**
+ * Set images based on type.
+ */
+
+    private void typeImageSetter(){
+        String type = ((RadioButton)findViewById(typeGroup.getCheckedRadioButtonId())).getText().toString();
+        if (type.equalsIgnoreCase(getString(R.string.deluxe))){
+            imageView.setImageResource(R.drawable.nydeluxe);
+        } else if (type.equalsIgnoreCase(getString(R.string.bbq_chicken))) {
+            imageView.setImageResource(R.drawable.nybbqchicken);
+        } else if (type.equalsIgnoreCase(getString(R.string.meatzza))) {
+            imageView.setImageResource(R.drawable.nymeatzza);
+        }else {
+            Toast.makeText(this, getString(R.string.error), Toast.LENGTH_SHORT).show();
+            imageView.setImageResource(R.drawable.pizza);
+        }
+    }
+
+
+/**
+ * Full image setter.
+ */
+    private void fullImageSetter(){
+        String style = ((RadioButton)findViewById(styleGroup.getCheckedRadioButtonId())).getText().toString();
+        if (style.equalsIgnoreCase(getString(R.string.new_york))){
+            typeImageSetter();
+        } else if (style.equalsIgnoreCase(getString(R.string.chicago))) {
+            String type = ((RadioButton)findViewById(typeGroup.getCheckedRadioButtonId())).getText().toString();
+            if (type.equalsIgnoreCase(getString(R.string.deluxe))){
+                imageView.setImageResource(R.drawable.chicagodeluxe);
+            } else if (type.equalsIgnoreCase(getString(R.string.bbq_chicken))) {
+                imageView.setImageResource(R.drawable.chicagobbqchicken);
+            } else if (type.equalsIgnoreCase(getString(R.string.meatzza))) {
+                imageView.setImageResource(R.drawable.chicagomeatzza);
+            }else {
+                Toast.makeText(this, getString(R.string.error), Toast.LENGTH_SHORT).show();
+                imageView.setImageResource(R.drawable.pizza);
+            }
+        } else {
+            Toast.makeText(this, getString(R.string.error), Toast.LENGTH_SHORT).show();
+            imageView.setImageResource(R.drawable.pizza);
+        }
+    }
+
+    private Pizza makePizza(){
+        PizzaFactory pizzaFactory;
+        Pizza currentPizza = null;
+        try{
+            String style = ((RadioButton)findViewById(styleGroup.getCheckedRadioButtonId())).getText().toString();
+            if (style.equalsIgnoreCase(getString(R.string.new_york))){
+                pizzaFactory = new NYPizza();
+            } else if (style.equalsIgnoreCase(getString(R.string.chicago))) {
+                pizzaFactory = new ChicagoPizza();
+            } else {
+                throw new RuntimeException();
+            }
+            String type = ((RadioButton)findViewById(typeGroup.getCheckedRadioButtonId())).getText().toString();
+            if (type.equalsIgnoreCase(getString(R.string.deluxe))){
+                currentPizza = pizzaFactory.createDeluxe();
+            } else if (type.equalsIgnoreCase(getString(R.string.bbq_chicken))) {
+                currentPizza = pizzaFactory.createBBQChicken();
+            } else if (type.equalsIgnoreCase(getString(R.string.meatzza))) {
+                currentPizza = pizzaFactory.createMeatzza();
+            }else {
+                throw new RuntimeException();
+            }
+            String size = ((RadioButton)findViewById(sizeGroup.getCheckedRadioButtonId())).getText().toString();
+            if (size.equalsIgnoreCase(getString(R.string.large))){
+                currentPizza.setSize(Size.LARGE);
+            } else if (size.equalsIgnoreCase(getString(R.string.medium))) {
+                currentPizza.setSize(Size.MEDIUM);
+            } else if (size.equalsIgnoreCase(getString(R.string.small))) {
+                currentPizza.setSize(Size.SMALL);
+            }else {
+                throw new RuntimeException();
+            }
+        } catch (RuntimeException e) {
+            Toast.makeText(this, getString(R.string.error), Toast.LENGTH_SHORT).show();
+        }
+        return currentPizza;
+    }
+
+    public void addToOrder(View view) {
+        Order order = OrderSingleton.getInstance().getCurrentOrder();
+        order.orderAdd(makePizza());
+        styleGroup.clearCheck();
+        typeGroup.clearCheck();
+        sizeGroup.clearCheck();
+        fieldsFilled();
+        Toast.makeText(this, getString(R.string.ordered), Toast.LENGTH_SHORT).show();
     }
 
     public void toMain(View view){
